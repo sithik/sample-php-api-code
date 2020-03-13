a8b9bb9d98b689ed9489f6d5f4a6ac441120e6fa
Sithik <sithik@usistech.com>
2020-03-13 19:06:50 +0530

    Sample End Point and Route added

--- sample-php-api-code/app/Http/Controllers/ExampleController.php
+++ sample-php-api-code/app/Http/Controllers/ExampleController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
@@ -14,7 +15,16 @@ class ExampleController extends Controller
         //
     }
 
-    public function testEndPoint(){
+    public function testEndPoint(Request $request){
+
+        $output = intval($request->item_1) + intval($request->item_2);
+        $output_json = array("data"=>$output,"message"=>"Successfully added");
+
+        $response['code'] = 200;
+        $response['status'] = "success";
+        $response['message'] = "Successfully added";
+        $response['data'] = $output;
+        return response()->json($response,$response['code']);
         
     }
     //
